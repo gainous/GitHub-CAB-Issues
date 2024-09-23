f2ee93bb8cbcf60aed07db8668e2fa8216b8fedd
Jason Gainous <jgainous@sharjah.ac.ae>
2024-09-23 16:16:04 +0400

    aught Mo the meaning of reality

--- Code by Awais/New Significant Results/Capability.R
+++ Code by Awais/New Significant Results/Capability.R
@@ -84,3 +84,158 @@ summary(Economic_Issues_SysLive)
 
 ####################################################################
 
+names(CAB_data_Recoded)
+table(CAB_data_Recoded$country)
+
+# Ensure the 'createdummies' package is installed
+install.packages("createdummies")
+
+# Load the createdummies library
+library(createdummies)
+
+# Create dummy variables for the 'country' variable
+CAB_data_Recoded <- createdummies(CAB_data_Recoded, "country", sep = "_")
+
+# View the updated dataset with the dummy variables
+head(CAB_data_Recoded)
+
+library(car)
+CAB_data_Recoded$KZ = recode(CAB_data_Recoded$country, "'KZ' = 1; else = 0")
+CAB_data_Recoded$KG = recode(CAB_data_Recoded$country, "'KG' = 1; else = 0")
+CAB_data_Recoded$GA = recode(CAB_data_Recoded$country, "'GA' = 1; else = 0")
+
+dom_issues_mod <- glm(Domestic_Issues ~  sm_critical_index + tv_critical_index + homophilous_index +
+                            + age_n_sc + edu_n_sc + inc_n_sc + pol_interest_n_sc + KZ + KG, 
+                               data = CAB_data_Recoded, 
+                               family = binomial)
+summary(dom_issues_mod)
+
+
+pv_issues_mod <- glm(Political_Violence ~  sm_critical_index + tv_critical_index + homophilous_index +
+                      + age_n_sc + edu_n_sc + inc_n_sc + pol_interest_n_sc + KZ + KG, 
+                      data = CAB_data_Recoded, 
+                      family = binomial)
+summary(pv_issues_mod)
+
+econ_issues_mod <- glm(Economic_Issues ~  sm_critical_index + tv_critical_index + homophilous_index +
+                      + age_n_sc + edu_n_sc + inc_n_sc + pol_interest_n_sc + KZ + KG, 
+                      data = CAB_data_Recoded, 
+                      family = binomial)
+summary(econ_issues_mod)
+
+#Variable Construction###############################################
+#Avoidance Index
+library(psy)
+avoidance_matrix = cbind(
+  CAB_data_Recoded$avoidance_blocking_n_sc,
+  CAB_data_Recoded$avoidance_unfriending_n_sc,
+  CAB_data_Recoded$avoidance_leaving_group_n_sc,
+  CAB_data_Recoded$avoidance_unsubscribing_n_sc)
+cronbach(avoidance_matrix)
+
+library(scales)
+CAB_data_Recoded$avoidance_index = rescale((
+  CAB_data_Recoded$avoidance_blocking_n_sc +
+    CAB_data_Recoded$avoidance_unfriending_n_sc +
+    CAB_data_Recoded$avoidance_leaving_group_n_sc +
+    CAB_data_Recoded$avoidance_unsubscribing_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$avoidance_index)
+
+#Exposure to Disagreement Index
+sm_disagreement_matrix = cbind(
+  CAB_data_Recoded$sm_disagreement_politics_n_sc,
+  CAB_data_Recoded$sm_disagreement_issues_n_sc,
+  CAB_data_Recoded$sm_disagreement_news_n_sc)
+cronbach(sm_disagreement_matrix)
+
+CAB_data_Recoded$sm_disagreement_index = rescale((
+  CAB_data_Recoded$sm_disagreement_politics_n_sc +
+    CAB_data_Recoded$sm_disagreement_issues_n_sc +
+    CAB_data_Recoded$sm_disagreement_news_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$sm_disagreement_index)
+
+#Social Media Critical Exposure/Engagement Index
+sm_critical_matrix = cbind(
+  CAB_data_Recoded$sm_critical_local_n_sc,
+  CAB_data_Recoded$sm_critical_central_n_sc,
+  CAB_data_Recoded$sm_engage_critical_n_sc)
+cronbach(sm_critical_matrix)
+
+CAB_data_Recoded$sm_critical_index = rescale((
+  CAB_data_Recoded$sm_critical_local_n_sc +
+    CAB_data_Recoded$sm_critical_central_n_sc +
+    CAB_data_Recoded$sm_engage_critical_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$sm_critical_index)
+
+#Television Critical Exposure Index
+tv_critical_matrix = cbind(
+  CAB_data_Recoded$tv_critical_local_n_sc,
+  CAB_data_Recoded$tv_critical_central_n_sc)
+cronbach(tv_critical_matrix)
+
+CAB_data_Recoded$tv_critical_index = rescale((
+  CAB_data_Recoded$tv_critical_local_n_sc +
+    CAB_data_Recoded$tv_critical_central_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$tv_critical_index)
+
+#Pro Russia Digital Exposure Index
+dig_pro_russia_matrix = cbind(
+  CAB_data_Recoded$sm_positive_local_n_sc,
+  CAB_data_Recoded$sm_positive_central_n_sc,
+  CAB_data_Recoded$sm_engage_supportive_sc,
+  CAB_data_Recoded$clickable_state_n_sc,
+  CAB_data_Recoded$clickable_russian_n_sc,
+  CAB_data_Recoded$paid_posters_percentage_n_sc)
+cronbach(dig_pro_russia_matrix)
+
+CAB_data_Recoded$dig_pro_russia_index = rescale((
+  CAB_data_Recoded$sm_positive_local_n_sc +
+    CAB_data_Recoded$sm_positive_central_n_sc +
+    CAB_data_Recoded$sm_engage_supportive_sc +
+    CAB_data_Recoded$clickable_state_n_sc +
+    CAB_data_Recoded$clickable_russian_n_sc +
+    CAB_data_Recoded$paid_posters_percentage_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$dig_pro_russia_index)
+
+CAB_data_Recoded$sm_disagreement_politics_n_sc = rescale(CAB_data_Recoded$sm_disagreement_politics_n, to = c(1, 0))
+CAB_data_Recoded$sm_disagreement_news_n_sc = rescale(CAB_data_Recoded$sm_disagreement_news_n, to = c(1, 0))
+CAB_data_Recoded$sm_disagreement_issues_n_sc = rescale(CAB_data_Recoded$sm_disagreement_issues_n, to = c(1, 0))
+
+#Homophilous Network
+homophilous_matrix = cbind(
+  CAB_data_Recoded$avoidance_blocking_n_sc,
+  CAB_data_Recoded$avoidance_unfriending_n_sc,
+  CAB_data_Recoded$avoidance_leaving_group_n_sc,
+  CAB_data_Recoded$avoidance_unsubscribing_n_sc,
+  CAB_data_Recoded$sm_disagreement_politics_n_sc,
+  CAB_data_Recoded$sm_disagreement_issues_n_sc,
+  CAB_data_Recoded$sm_disagreement_news_n_sc,
+  CAB_data_Recoded$echo_chamber_n_sc)
+cronbach(homophilous_matrix)
+
+library(scales)
+CAB_data_Recoded$homophilous_index = rescale((
+  CAB_data_Recoded$avoidance_blocking_n_sc +
+  CAB_data_Recoded$avoidance_unfriending_n_sc +
+  CAB_data_Recoded$avoidance_leaving_group_n_sc +
+  CAB_data_Recoded$avoidance_unsubscribing_n_sc +
+  CAB_data_Recoded$sm_disagreement_politics_n_sc +
+  CAB_data_Recoded$sm_disagreement_issues_n_sc +
+  CAB_data_Recoded$sm_disagreement_news_n_sc +
+  CAB_data_Recoded$echo_chamber_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$homophilous_index)
+
+hist(CAB_data_Recoded$homophilous_index)
+#Exposure to Disagreement Index
+sm_disagreement_matrix = cbind(
+  CAB_data_Recoded$sm_disagreement_politics_n_sc,
+  CAB_data_Recoded$sm_disagreement_issues_n_sc,
+  CAB_data_Recoded$sm_disagreement_news_n_sc)
+cronbach(sm_disagreement_matrix)
+
+CAB_data_Recoded$sm_disagreement_index = rescale((
+  CAB_data_Recoded$sm_disagreement_politics_n_sc +
+    CAB_data_Recoded$sm_disagreement_issues_n_sc +
+    CAB_data_Recoded$sm_disagreement_news_n_sc), to = c(0, 1))
+summary(CAB_data_Recoded$sm_disagreement_index)
+
--- Code by Awais/Recoded Issues Into Three Categories.R
+++ Code by Awais/Recoded Issues Into Three Categories.R
@@ -13,3 +13,33 @@ table(CAB_data_Recoded$Economic_Issues)
 
 library(writexl)
 write_xlsx(CAB_data_Recoded, "CAB_data_Recoded.xlsx")
+
+CAB_data_Recoded$Unresolved_territorial_conflicts_Combined
+
+library(Rcmdr)
+
+names(CAB_data_Recoded)
+library(Hmisc, pos=19)
+rcorr.adjust(CAB_data_Recoded[,c("tv_critical_index",
+                                 "Political_Instability_Combined",                                                  
+                                 "Unemployment_Combined",                                                           
+                                 "Inflation_or_Prices_Combined" ,                                                   
+                                 "Wages_or_pensions_Combined"  ,                                                    
+                                 "Taxes_Combined"        ,                                                          
+                                 "Access_to_basic_needs_Combined" ,                                                 
+                                 "General_economic_situation_Combined"   ,                                          
+                                 "education_Combined"   ,                                                           
+                                 "Housing_Combined"   ,                                                             
+                                 "Terrorism_Combined"     ,                                                         
+                                 "War_or_conflict_abroad_Combined"   ,                                              
+                                 "Personal_safety_Combined"  ,                                                      
+                                 "Corruption_Combined" ,                                                            
+                                 "Lack_of_opportunities_Combined"  ,                                                
+                                 "ethnic_or_religious_tensions_Combined" ,                                          
+                                 "Infrastructure_Combined"     ,                                                    
+                                 "Environmental_change_Combined"  ,                                                 
+                                 "Unresolved_territorial_conflicts_Combined"  ,                                     
+                                 "Emigration_Combined"    )], 
+             type="pearson", use="complete")
+
+     
