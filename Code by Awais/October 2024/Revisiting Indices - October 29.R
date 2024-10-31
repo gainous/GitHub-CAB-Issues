77d07f59ac47b289fa6acf72bfb9f402439242e2
Muhammad Awais <122136112+awwaiis@users.noreply.github.com>
2024-10-31 09:47:21 +0400

    Updates

--- Code by Awais/October 2024/Revisiting Indices - October 29.R
+++ Code by Awais/October 2024/Revisiting Indices - October 29.R
@@ -1,7 +1,34 @@
+save.image("~/GitHub/GitHub-CAB-Issues/CAB_data.RData")
+
+# Political instability, Corruption, Discrimination/ethnic or religious tensions####
+pol_issues <- ifelse(CAB_data_dem$q13_a %in% c(1, 13, 15) | CAB_data_dem$q13_b %in% c(1, 13, 15), 1, 0)
+
+# Unemployment, Inflation/Prices, Wages/Pensions, Lack of opportunities, General economic situation, Access to basic needs (food, electricity, medicine, etc)####
+eco_issues <- ifelse(CAB_data_dem$q13_a %in% c(2, 3, 4, 6, 7, 14) | CAB_data_dem$q13_b %in% c(2, 3, 4, 6, 7, 14), 1, 0)
+
+# Deterioration of the quality and access to education, Increase in crime/violence/concerns about personal safety, Emigration####
+soc_issues <- ifelse(CAB_data_dem$q13_a %in% c(8, 12, 19) | CAB_data_dem$q13_b %in% c(8, 12, 19), 1, 0)
+
+# Terrorism, War/conflict in other countries, Unresolved territorial conflicts####
+sec_issues <- ifelse(CAB_data_dem$q13_a %in% c(10, 11, 18) | CAB_data_dem$q13_b %in% c(10, 11, 18), 1, 0)
+
+# Deterioration of the environment, Infrastructure issues###
+env_issues <- ifelse(CAB_data_dem$q13_a %in% c(8, 16) | CAB_data_dem$q13_b %in% c(8,16), 1, 0)
+
+table(pol_issues)
+table(eco_issues)
+table(soc_issues)
+table(sec_issues)
+table(sec_issues)
+table(env_issues)
+
+
 #Critical Social Media Index
 critical_social_media = cbind(
   CAB_data_dem$sm_critical_local_n_sc,
   CAB_data_dem$sm_critical_central_n_sc)
+
+library(psy)
 cronbach(critical_social_media)
 
 library(scales)
@@ -38,7 +65,7 @@ sm_pol_news = rescale(
   to = c(1, 0)
 )
 
-#Positive SM Media Flow
+#Positive SM Media
 sm_positive = rescale(
   CAB_data_dem$sm_positive_local_n_sc +
   CAB_data_dem$sm_positive_central_n_sc,
@@ -82,21 +109,15 @@ gen_sm_index = cbind(
   CAB_data_dem$vkontakte_n_sc,
   CAB_data_dem$instagram_n_sc,
   CAB_data_dem$tiktok_n_sc,
-  CAB_data_dem$twitter_n_sc,
-  CAB_data_dem$youtube_n_sc,
-  CAB_data_dem$whatsapp_n_sc, 
-  CAB_data_dem$telegram_n_sc)
-cronbach(sm_platform_index)
+  CAB_data_dem$twitter_n_sc)
+cronbach(gen_sm_index)
 
 gen_sm_index <- rescale(
   CAB_data_dem$facebook_n_sc +
   CAB_data_dem$vkontakte_n_sc +
   CAB_data_dem$instagram_n_sc +
   CAB_data_dem$tiktok_n_sc +
-  CAB_data_dem$twitter_n_sc +
-  CAB_data_dem$youtube_n_sc +
-  CAB_data_dem$whatsapp_n_sc + 
-  CAB_data_dem$telegram_n_sc,
+  CAB_data_dem$twitter_n_sc,
   to = c(0, 1)
 )
 
@@ -160,9 +181,12 @@ national_defenders = cbind(CAB_data_dem$system_capable_n_sc,
                            CAB_data_dem$participate_rally_nat_def,
                            CAB_data_dem$participate_meeting_nat_def,
                            CAB_data_dem$participate_member_nat_def)
-library(psy)
+
 cronbach(national_defenders)
 
+CAB_data_dem$participate_rally_nat_def = rescale(CAB_data_dem$participate_rally_n, to = c(1, 0)) # 1 means yes
+CAB_data_dem$participate_meeting_nat_def = rescale(CAB_data_dem$participate_meeting_n, to = c(1, 0)) # 1 means yes
+CAB_data_dem$participate_member_nat_def = rescale(CAB_data_dem$participate_member_n, to = c(1, 0)) # 1 means not at all
 
 national_defenders = rescale(CAB_data_dem$system_capable_n_sc +
                            CAB_data_dem$system_proud_n_sc +
@@ -214,6 +238,14 @@ to = c(1, 0)
 
 
 
+
+CAB_data_dem$tracking_central_sur_ave_lib =  rescale(CAB_data_dem$tracking_central_n, to = c(1, 0)) #1 means not comfortable at all
+CAB_data_dem$tracking_local_sur_ave_lib =  rescale(CAB_data_dem$tracking_local_n, to = c(1, 0)) #1 means not comfortable at all
+CAB_data_dem$tracking_companies_sur_ave_lib =  rescale(CAB_data_dem$tracking_companies_n, to = c(1, 0)) #1 means not comfortable at all
+CAB_data_dem$sm_engage_friends_sur_ave_lib =  rescale(CAB_data_dem$sm_engage_friends_n, to = c(0, 1)) # 1 means less engagement
+CAB_data_dem$sm_engage_groups_sur_ave_lib = rescale(CAB_data_dem$sm_engage_groups_n, to = c(0, 1)) # 1 means less engagement
+
+
 surv_averse_lib = cbind(
   CAB_data_dem$tracking_central_sur_ave_lib,
   CAB_data_dem$tracking_local_sur_ave_lib,
@@ -238,3 +270,12 @@ CAB_data_dem$surv_averse_lib_index <- rescale(
   # CAB_data_dem$trust_western_n_sc,
   to = c(0, 1)
 )
+
+
+
+
+for (dep_var in dependent_vars) {
+  formula <- as.formula(paste(dep_var, "~ facebook_n_sc + instagram_n_sc + twitter_n_sc + vkontakte_n_sc + 
+                               age_n_sc + gender_Male + urbanicity_City + edu_n_sc + inc_n_sc"))
+  models[[dep_var]] <- glm(formula, data = CAB_data_dem, family = binomial)
+}
